3a6398ef27a3b045c8d8990ab408d82a96a6805a
emiliopg47 <4803960@alu.murciaeduca.es>
2025-04-09 16:09:20 +0200

    Comprobación de errores del login

--- Servidor-multijugador/src/main/java/com/emilio/servidor_multijugador/persistencia/servicios/ServiceUsuario.java
+++ Servidor-multijugador/src/main/java/com/emilio/servidor_multijugador/persistencia/servicios/ServiceUsuario.java
@@ -2,7 +2,7 @@ package com.emilio.servidor_multijugador.persistencia.servicios;
 
 import com.emilio.servidor_multijugador.persistencia.repository.UsuarioRepository;
 import com.emilio.servidor_multijugador.persistencia.modelos.Usuario;
-import jakarta.annotation.PostConstruct;
+import com.emilio.servidor_multijugador.web.apirest.response.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,11 +24,19 @@ public class ServiceUsuario {
     public void deleteById(Integer id) {
         dao.deleteById(id);
     }
-    public Usuario findByNick(String nick, String password) {
-        return dao.findByNickAndPassword(nick,password);
+    public boolean findByNick(String nick) {
+        return dao.findByNick(nick) ;
     }
 
-    public Usuario findByCorreo(String correo, String password) {
+    public boolean findByCorreo(String correo) {
+        return dao.findByCorreo(correo);
+    }
+
+    public Usuario findByCorreoAndPassword(String correo, String password) {
+        return dao.findByCorreoAndPassword(correo,password);
+    }
+
+    public Usuario findByNickAndPassword(String correo, String password) {
         return dao.findByCorreoAndPassword(correo,password);
     }
 

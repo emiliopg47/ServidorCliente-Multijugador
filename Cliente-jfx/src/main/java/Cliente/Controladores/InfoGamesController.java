93f067bcfbdcdfba4f8580f5b3412d1993a28bca
emiliopg47 <4803960@alu.murciaeduca.es>
2025-05-16 12:33:22 +0200

    Seccion de informacion de los juegos

---
+++ Cliente-jfx/src/main/java/Cliente/Controladores/InfoGamesController.java
@@ -0,0 +1,39 @@
+package Cliente.Controladores;
+
+import Config.APIREQUEST;
+import Config.CONFIG;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import org.json.JSONObject;
+
+
+public class InfoGamesController extends Controller{
+
+    @FXML
+    public ComboBox<String> gameComboBox;
+
+    @FXML
+    public TextArea descripcionArea;
+
+    @FXML
+    public TextArea comoJugarArea;
+
+    @FXML
+    public void initialize() {
+        gameComboBox.getItems().addAll(CONFIG.listaJuegos);
+        gameComboBox.setOnAction(event -> mostrarInformacionJuego(gameComboBox.getValue()));
+    }
+
+    public void mostrarInformacionJuego(String juego) {
+        JSONObject json = getApi(APIREQUEST.GAME_INFO + "/" + juego);
+        System.out.println(json);
+        String descripcion =json.getString("descripcion");
+        String comoJugar = json.getString("comoJugar");
+
+        descripcionArea.setText(descripcion);
+        comoJugarArea.setText(comoJugar);
+    }
+
+
+}

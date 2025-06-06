29b7480cc55abfc6098d66ec9f98a1106f134924
emiliopg47 <4803960@alu.murciaeduca.es>
2025-06-06 11:58:53 +0200

    Changes

--- esquema.sql
+++ esquema.sql
@@ -1,42 +1,110 @@
--- Tabla de usuarios
-CREATE TABLE usuario (
-    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nick VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    correo VARCHAR(100) UNIQUE NOT NULL,
-    fecha_nac DATE NOT NULL
-);
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: TFGdb
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.24.04.1
 
--- Tabla de juegos
-CREATE TABLE juego (
-    id_juego INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) UNIQUE NOT NULL,
-    descripcion TEXT
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Tabla del historial de partidas jugadas
-CREATE TABLE historial_games (
-    id_game INT PRIMARY KEY AUTO_INCREMENT,
-    id_jugador1 INT NOT NULL,
-    id_jugador2 INT NOT NULL,
-    id_juego INT NOT NULL,
-    fecha_hora DATETIME NOT NULL,
-    puntos_j1 INT NOT NULL,
-    puntos_j2 INT NOT NULL,
-    winner TINYINT, -- 1: jugador1, 2: jugador2, 0: empate, NULL: sin determinar
+--
+-- Table structure for table `historial_games`
+--
 
-    FOREIGN KEY (id_jugador1) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_jugador2) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
-);
+DROP TABLE IF EXISTS `historial_games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial_games` (
+  `id_game` int NOT NULL AUTO_INCREMENT,
+  `id_jugador1` int NOT NULL,
+  `id_jugador2` int NOT NULL,
+  `duracionSeg` bigint DEFAULT NULL,
+  `id_juego` int NOT NULL,
+  `fecha` date NOT NULL,
+  `puntos_j1` int NOT NULL,
+  `puntos_j2` int NOT NULL,
+  `winner` tinyint DEFAULT NULL,
+  `duracion_seg` bigint DEFAULT NULL,
+  PRIMARY KEY (`id_game`),
+  KEY `id_jugador1` (`id_jugador1`),
+  KEY `id_jugador2` (`id_jugador2`),
+  KEY `id_juego` (`id_juego`),
+  CONSTRAINT `historial_games_ibfk_1` FOREIGN KEY (`id_jugador1`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `historial_games_ibfk_2` FOREIGN KEY (`id_jugador2`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `historial_games_ibfk_3` FOREIGN KEY (`id_juego`) REFERENCES `juego` (`id_juego`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Tabla de ranking por usuario y juego
-CREATE TABLE ranking (
-    id_usuario INT NOT NULL,
-    id_juego INT NOT NULL,
-    puntos INT NOT NULL DEFAULT 0,
+--
+-- Table structure for table `juego`
+--
 
-    PRIMARY KEY (id_usuario, id_juego),
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
-);
+DROP TABLE IF EXISTS `juego`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juego` (
+  `id_juego` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` tinytext,
+  `como_jugar` tinytext,
+  PRIMARY KEY (`id_juego`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ranking`
+--
+
+DROP TABLE IF EXISTS `ranking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ranking` (
+  `id_usuario` int NOT NULL,
+  `id_juego` int NOT NULL,
+  `puntos` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_usuario`,`id_juego`),
+  KEY `id_juego` (`id_juego`),
+  CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `ranking_ibfk_2` FOREIGN KEY (`id_juego`) REFERENCES `juego` (`id_juego`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nick` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `imagen` mediumblob,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `nick` (`nick`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-06 11:56:54
